10dcd714943f6d1f53146599ee303d42cef6cd64
dongguanghua <dongguanghua@ztgame.com>
2019-06-26 19:18:25 +0800

    添加测试文件

--- ts/ts1.py
+++ ts/ts1.py
@@ -2,8 +2,17 @@
 # __author__ = 'doc007'
 
 
-if __name__ == '__main__':
+import os
+import sys
+from pyplusplus import module_builder
 
-    a = zip((1,2,3), (1, 2,4), (1, 2,5))
-    b = list(a)
-    pass
+mb = module_builder.module_builder_t(
+    files=['chg.h'],
+    xml_generator_path='D:\\GCC_XML\\bin\\gccxml.exe'
+)
+
+mb.build_code_creator(module_name='libchg_py')  # 要生成的python模块的名称
+
+mb.code_creator.user_defined_directories.append(os.path.abspath('.'))
+
+mb.write_module(os.path.join(os.path.abspath('.'), 'chg_py.cc'))  # 要生成的boost.python封装好的代码文件的名称
